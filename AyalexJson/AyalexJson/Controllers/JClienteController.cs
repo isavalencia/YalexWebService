8a588bfefa3c529efa63b2f384f6f9137f751fc7
isavalencia <105090893+isavalencia@users.noreply.github.com>
2023-06-09 17:13:26 -0600

    subido

---
+++ AyalexJson/AyalexJson/Controllers/JClienteController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AyalexJson.Models;
+namespace AyalexJson.Controllers
+{
+    public class JClienteController : Controller
+    {
+        
+        // GET: JCliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult ObtenerCli()
+        {
+            //List<cliente> lista;
+            using(yalexrentacarEntities db = new yalexrentacarEntities())
+            {
+               var lista = (from d in db.clientes 
+                       select  new { d.id_cliente, d.nombres, d.apellidos, d.direccion, d.telefono, d.correo, d.licencia, d.dui, d.estado, d.contrasena, d.rol}).ToList();
+
+                return Json(lista, JsonRequestBehavior.AllowGet);
+            }
+
+           
+        }
+    }
+}
---
+++ AyalexJson/AyalexJson/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AyalexJson.MvcApplication" Language="C#" %>
